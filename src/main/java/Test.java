8f3420b23bca4aaabc9a275a8d6b95eb99e6daae
praveenkumarkumaresan <praveenresponsible@gmail.com>
2022-03-20 09:13:56 -0500

    first day of marathon

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    private static void test(){
+        Map<Integer,Integer> testMap = new HashMap<>();
+
+    }
+    public static void main(String[] args){
+        System.out.println("fun starts here");
+        test();
+    }
+}
