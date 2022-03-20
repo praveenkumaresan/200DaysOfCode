56a17142a78bdaf5eed3886049892c86d1266bc3
praveenkumarkumaresan <praveenresponsible@gmail.com>
2022-03-20 11:07:44 -0500

    Day1 problem 2 subsequence

--- src/main/java/ValidateSubSequence.java
+++ src/main/java/ValidateSubSequence.java
@@ -1,3 +1,18 @@
+import java.util.List;
+
 public class ValidateSubSequence {
-    
+
+    public static boolean checkSequenceMethod(List<Integer> mainArray, List<Integer> sequenceArray){
+        int mainArrayIndex = 0;
+        int sequenceArrayIndex = 0;
+
+        while (mainArrayIndex < mainArray.size() && sequenceArrayIndex < sequenceArray.size()){
+            if (mainArray.get(mainArrayIndex).equals(sequenceArray.get(sequenceArrayIndex))){
+                sequenceArrayIndex++;
+            }
+            mainArrayIndex++;
+        }
+
+        return sequenceArrayIndex == sequenceArray.size();
+    }
 }
